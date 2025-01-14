30cc02c779a9a61e45280e10d0a8ac08978ae562
Lakshan Imantha <lakshan.imantha02@gmail.com>
2025-01-15 01:21:42 +0530

    Delete React files directory

--- React files/src/components/PrivateRoutes/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoutes = ({ element }) => {
-    const user = localStorage.getItem("user"); 
-
-    return user ? element : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoutes;
